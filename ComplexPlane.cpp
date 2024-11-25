77e69fb04a58051a9b2a54d4370a51e3bfc68fc1
AlinaKralya04 <alinakralya04@gmail.com>
2024-11-24 17:56:38 -0800

    Update Constructor

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -4,11 +4,14 @@ ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
 {
 	m_pixel_size = { pixelWidth, pixelHeight };
 	m_aspectRatio = (float)pixelHeight / (float)pixelWidth;
+
 	m_plane_center = { 0,0 };
 	m_plane_size = { BASE_WIDTH, BASE_HEIGHT * m_aspectRatio };
 	m_zoomCount = 0;
 	m_state = State::CALCULATING;
-	VertexArray m_vArray(Points, pixelWidth * pixelHeight);
+
+	m_vArray.setPrimitiveType(Points);
+	m_vArray.resize(pixelWidth * pixelHeight);
 }
 
 void ComplexPlane::draw(RenderTarget& target, RenderStates states) const 
