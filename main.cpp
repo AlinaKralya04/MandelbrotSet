59ad22d3e8c5ba036a416f2955b4771748a09af1
AlinaKralya04 <alinakralya04@gmail.com>
2024-11-24 20:44:26 -0800

    Update VideoMode and Input

--- main.cpp
+++ main.cpp
@@ -16,58 +16,53 @@ int main()
     RenderWindow window(vm, "Mandelbrot Set", Style::Default);
 
     //Construct the ComplexPlane
-    ComplexPlane plane;
+    ComplexPlane plane(pixelWidth, pixelHeight);
 
     //Construct the Font and Text objects
     Text text;
     Font font;
+
     font.loadFromFile("SparkyStonesRegular-BW6ld.ttf");
     text.setFont(font);
-    text.setCharacterSize(30); 
+
+    text.setCharacterSize(20);
     text.setFillColor(Color::White);
     text.setStyle(Text::Italic);
-    text.setPosition(400.f, 200.f);
+    text.setPosition(10.0, 10.0);
 
     while (window.isOpen())
 	{
         Event event;
-        while (window.pollEvent(event))
+while (window.pollEvent(event))
+{
+    // handle input segment
+    Vector2i current_pos(event.mouseButton.x, event.mouseButton.y);
+    if (event.type == Event::Closed)
+    {
+        // Quit the game when the window is closed
+        window.close();
+    }
+    if (event.type == sf::Event::MouseButtonPressed)
+    {
+        if (event.mouseButton.button == sf::Mouse::Left)
         {
-            // handle input segment?
-            if (event.type == Event::Closed)
-            {
-                // Quit the game when the window is closed
-                window.close();
-            }
-            if (event.type == sf::Event::MouseButtonPressed)
-            {
-                if (event.mouseButton.button == sf::Mouse::Left)
-                {
-                    plane.zoomIn();
-                    plane.setCenter();
-                    std::cout << "the left button was pressed" << std::endl;
-                    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
-                    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
-                }
-                else if (event.mouseButton.button == sf::Mouse::Right)
-                {
-                    plane.zoomOut();
-                    plane.setCenter();
-                    std::cout << "the right button was pressed" << std::endl;
-                    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
-                    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
-                }
-            }
-            if (event.type == sf::Event::MouseMoved)
-            {
-                plane.setMouseLocation();
-                std::cout << "new mouse x: " << event.mouseMove.x << std::endl;
-                std::cout << "new mouse y: " << event.mouseMove.y << std::endl;
-            }
-            if (Keyboard::isKeyPressed(Keyboard::Escape))
-            {
-                window.close();
-            }
+            plane.zoomIn();
+            plane.setCenter(current_pos);
+        }
+        else if (event.mouseButton.button == sf::Mouse::Right)
+        {
+            plane.zoomOut();
+            plane.setCenter(current_pos);
+        }
+    }
+    if (event.type == sf::Event::MouseMoved)
+    {
+        plane.setMouseLocation(current_pos);
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Escape))
+    {
+        window.close();
+    }
             //update scene segment
             plane.updateRender();
             plane.loadText();
